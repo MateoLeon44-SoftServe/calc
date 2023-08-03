754eb96b84a9982e834023e79c51c4998e23be3a
MateoLeon44-SoftServe <maleon@softserveinc.com>
2023-08-03 12:29:58 -0500

    Added vite

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "url";
+
+export default {
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  base: "https://mateoleon44-softserve.github.io/calc/",
+};
