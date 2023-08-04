103af6f6a990accc527b95c9faf88bb740082543
MateoLeon44-SoftServe <maleon@softserveinc.com>
2023-08-03 22:18:28 -0500

    Added dotenv and change config file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "url";
 
+const SILENT = Boolean(process.env.SILENT) ?? false;
+
 export default {
   plugins: [vue()],
+  root: process.cwd(),
+  publicDir: "publicDir",
+  logLevel: SILENT ? "error" : "info",
+  chunkSizeWarningLimit: 500,
+  define: {
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: "esnext",
   base: "https://mateoleon44-softserve.github.io/calc",
 };
